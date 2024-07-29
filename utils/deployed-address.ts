83c04d5da4697030f57826e373c078120cab707d
Kehed10 <ahmadrendi1994@gmail.com>
2024-07-29 16:56:42 +0000

    update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0xa62690E2608175Eb575755A25acE574905A9A4DC'
+export const ProxyAdmin = '0x16984F1e97dc844e14807298a31A5F46BDB25A2d'
+export const SWTRImplementation = '0x666ACB7eFfeD9c808c9c5A75aE156Fe16bd558F3'
     
